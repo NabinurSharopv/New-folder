c7efffed86d299b43c0ead4033024434a8b75ab6
nabinur <nabinursh123@gmail.com>
2025-12-14 19:09:58 +0500

    qilindi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,41 @@
-import { useDispatch, useSelector } from "react-redux"
-import {addTodo , deleteTodo  } from ".todoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo } from "./todoSlice";
 import { useState } from "react";
 
-const App = () => {
-const  [ text , setText ] = useState("");
-const todos = useSelector((state)  => state.todo);
-const dispatch = useDispatch();
+function App() {
+  const [text, setText] = useState("");
+  const todos = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
+
   return (
-    <>
-      <h3>Todo Lits</h3>  
-    
-    <input value={text}  onChange={(e) setText(e.target.value)} />
-   <button   onClick={() => { dispatch(addTodo(text)); setText("");}} >Add</button>
-     <ul>
-       {
-        todos.map((t) => {
+    <div>
+      <h3>To-Do List</h3>
+
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          dispatch(addTodo(text));
+          setText("");
+        }}
+      >
+        Add
+      </button>
+
+      <ul>
+        {todos.map((t) => (
           <li key={t.id}>
             {t.text}
-            <button onClick={() =>  dispatch(deleteTodo (t.id))}>
-              x
+            <button onClick={() => dispatch(deleteTodo(t.id))}>
+              X
             </button>
           </li>
-        })
-       }
-     </ul>
-   </>
-  )
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/features/todo/todoSlice.js
+++ vite-project/src/features/todo/todoSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
-    name: "todo",
-    initialState: [],
-    reducers: {
-        addTodo: (state, action) => {
-            state.push ({
-                id: Data.now(),
-                text: action.payload,
-            });
-        },
-        deleteTodo: (state, action) => {
-            return state.filter((t) => t.d.id !== action.payload);
-        },
+  name: "todo",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({
+        id: Date.now(),
+        text: action.payload,
+      });
     },
-})
+    deleteTodo: (state, action) => {
+      return state.filter((t) => t.id !== action.payload);
+    },
+  },
+});
 
-export const  ( addTodo , deleteTodo ) = todoSlice.actions;
+export const { addTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
